46e1236f7f4b52b93cff98ea9f14be8eef531271
Muzammil62 <ibrahimmuzammil62@gmail.com>
2026-02-11 10:04:24 +0530

    Fix caesar_cipher_encryption function logic

--- 7.py
+++ 7.py
@@ -1,21 +1,21 @@
-def caesar_cipher_encryption(plain_text,shift):
-    encrypted_text=""
+def caesar_cipher_encryption(plain_text, shift):
+    encrypted_text = ""
 
     for char in plain_text:
         if char.isalpha():
             if char.isupper():
-                encrypted_char = chr((ord(char)- ord('A')+ shift)% 26 + ord('A'))
+                encrypted_char = chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
             else:
-                encrypted_char = chr((ord(char)- ord('a')+ shift)% 26 + ord('a'))
-                encrypted_char += encrypted_char
+                encrypted_char = chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            encrypted_text += encrypted_char
         else:
-                encrypted_text += char
+            encrypted_text += char
 
-                return encrypted_text
-        
-        plain_text = input+("enter the plaintext:")
-        shift = int(input("Enter the shift value:"))
+    return encrypted_text
 
-        encrypted_text = caesar_cipher_encryption(plain_text, shift)
 
-        print("encrypted text:", encrypted_text) 
+plain_text = input("Enter the plaintext: ")
+shift = int(input("Enter the shift value: "))
+
+encrypted_text = caesar_cipher_encryption(plain_text, shift)
+print("Encrypted text:", encrypted_text)
